09d1e2a734feb8a8b81c61e6a53e06309831d1c0
Joan Maina <mainajoan555@gmail.com>
2025-11-14 18:51:56 +0300

    the preprocessing

--- huggingface/main.py
+++ huggingface/main.py
@@ -13,3 +13,16 @@ CLASS_NAMES = [Acne,Carcinoma ,Eczema, Keratosis ,Milia ,Rosacea] #Just in the o
 
 #define input size
 INPUT_SIZE = (224,224) #input size of your model
+
+# now the prediction function
+def predict_skin(image):
+    # Preprocess the image
+    image = image.resize(INPUT_SIZE)
+    image = np.array(image) / 255.0  # Normalize to [0, 1] to make it a smaller number 
+    image = np.expand_dims(image, axis=0)  # Add batch dimension Because the model architecture itself was built around batches from the beginning — not just during training.
+
+    # Make prediction
+    predictions = model.predict(image)
+    predicted_class = CLASS_NAMES[np.argmax(predictions)]
+
+    return predicted_class
