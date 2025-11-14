0d167842ba3a337b1924d1905d53edffa5ab4421
Joan Maina <mainajoan555@gmail.com>
2025-11-14 18:23:24 +0300

    imports

---
+++ huggingface/main.py
@@ -0,0 +1,6 @@
+#imports
+import gradio as gr #this is what we will use for huggingface space,its our UI
+import tensorflow as tf # WE  WILL USE TENSORFLOW TO LOAD OUR MODEL
+import numpy as np # to cnvert images into arrrays
+from PIL import Image # to process images
+
